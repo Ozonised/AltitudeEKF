eb7eb9cf5b771ffb88c0c6b620f61c8046c58342
Farhan Khan <74808685+Ozonised@users.noreply.github.com>
2026-02-11 21:11:07 +0530

    feat: add ekf class, and methods

---
+++ AltitudeEKF.cpp
@@ -0,0 +1,9 @@
+/*
+ * AltitudeEKF.cpp
+ *
+ *  Created on: Feb 11, 2026
+ *      Author: farhan
+ */
+
+#include "AltitudeEKF.hpp"
+
---
+++ AltitudeEKF.hpp
@@ -0,0 +1,36 @@
+/*
+ * AltitudeEKF.hpp
+ *
+ *  Created on: Feb 11, 2026
+ *      Author: farhan
+ */
+
+#ifndef ALTITUDEEKF_HPP_
+#define ALTITUDEEKF_HPP_
+
+#include <math.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdint.h>
+
+class AltitudeEKF
+{
+	private:
+		float Xcap[2];		// state vector/predicted state
+		float X[2];			// estimated state
+		float Pcap[2][2];	// predicted covariance
+		float Q[2];			// Process noise
+		float P[2][2];		// Estimated covariance
+		float R;			// Measurement noise (barometers noise)
+	public:
+		AltitudeEKF();
+		void SetPredictedState(float accelAlt, float accelVelocity);
+		void SetPredictedCovariance(float P00, float P11, float P22, float P33);
+		void SetProcessNoise(float accelAlt, float accelVelocity);
+		void SetMeasurementNoise(float val);
+		float GetAltitude(void);
+		bool Run(float accelerationZ, float baroAltitude);
+};
+
+
+#endif /* ALTITUDEEKF_ALTITUDEEKF_HPP_ */
